72de9d211b0842d0d0a67fee61085035e5e93d84
Jiahao Lu <lujjjh@gmail.com>
2022-07-07 20:14:58 +0800

    Add description

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,11 @@ import Head from 'next/head'
 const Home: NextPage = () => (
   <>
     <Head>
-      <title>LinearMouse</title>
+      <title>LinearMouse | The mouse and trackpad utility on macOS.</title>
+      <meta
+        name="description"
+        content="Customize mouse and trackpad's scrolling direction, pointer acceleration, pointer speed and so on..."
+      />
     </Head>
 
     <Header />
