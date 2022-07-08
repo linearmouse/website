e7ffb638a510f9ea08cb5bf51305564e1716bc28
Jiahao Lu <lujjjh@gmail.com>
2022-07-08 13:19:27 +0800

    Update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
         </Head>
         <body>
