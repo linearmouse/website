268eade3ca987e442de90695d0221f6bf558f64a
Jiahao Lu <lujjjh@gmail.com>
2022-07-17 15:00:46 +0800

    Integrate with Google Analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,19 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=block" rel="stylesheet" />
+
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-HDDCHC7YZH"></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-HDDCHC7YZH');
+`
+            }}
+          />
         </Head>
         <body>
           <Main />
