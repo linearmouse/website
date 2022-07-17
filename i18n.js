061b246eb5a5b8af144a5702f6f4f0c1dd0733b4
Jiahao Lu <lujjjh@gmail.com>
2022-07-17 10:57:38 +0800

    Update locales

--- i18n.js
+++ i18n.js
@@ -1,11 +1,37 @@
-const workaround = require('next-translate/lib/cjs/plugin/utils.js')
-
-workaround.defaultLoader =
-  '(l, n) => import(`@next-translate-root/locales/${l.replace(/-.+/, x => x.toUpperCase())}/${n}.json`).then(m => m.default)'
-
 module.exports = {
   defaultLocale: 'en',
-  locales: ['en', 'zh-cn'],
+  locales: [
+    'af-ZA',
+    'ar-SA',
+    'ca-ES',
+    'cs-CZ',
+    'da-DK',
+    'de-DE',
+    'el-GR',
+    'en',
+    'es-ES',
+    'fi-FI',
+    'fr-FR',
+    'he-IL',
+    'hu-HU',
+    'it-IT',
+    'ja-JP',
+    'ko-KR',
+    'nl-NL',
+    'no-NO',
+    'pl-PL',
+    'pt-BR',
+    'pt-PT',
+    'ro-RO',
+    'ru-RU',
+    'sr-SP',
+    'sv-SE',
+    'tr-TR',
+    'uk-UA',
+    'vi-VN',
+    'zh-CN',
+    'zh-TW'
+  ],
   pages: {
     '*': ['common'],
     '/': ['index']
