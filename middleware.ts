cb91d734a479e5cb89ada78cef53309b45e01a83
Jiahao Lu <lujjjh@gmail.com>
2022-07-18 13:33:53 +0800

    Revert "/zh-cn/ → /zh-CN/"
    This reverts commit edc52fc2e2c2aad6d063ec9f638186590cd0981e.

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextMiddleware, NextResponse } from 'next/server'
-
-export const middleware: NextMiddleware = async (request) => {
-  if (new URL(request.url).pathname === '/zh-cn/') {
-    return NextResponse.redirect(request.url.replace(/\/zh-cn\//, '/zh-CN/'), { status: 308 })
-  }
-
-  return undefined
-}
