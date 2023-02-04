cd74c3b32ea9b32f50661fd78e57c7cf7cb52069
Jiahao Lu <lujjjh@gmail.com>
2023-02-04 21:51:24 +0800

    CN: Integrated with Baidu analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,7 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
 import Script from 'next/script'
 import { ServerStyleSheet } from 'styled-components'
+import { isMainlandChinaCluster } from 'utils/config'
 
 export default class MyDocument extends Document {
   render() {
@@ -11,25 +12,34 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-
-          <Script src="https://www.googletagmanager.com/gtag/js?id=G-HDDCHC7YZH" strategy="afterInteractive"></Script>
-          <Script
-            id="google-analytics"
-            strategy="beforeInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-HDDCHC7YZH');
+          {isMainlandChinaCluster && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?bea29ce25bf357abb82bdcaa58cff168";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
 `
-            }}
-          />
+              }}
+            />
+          )}
         </Head>
         <body>
           <Main />
           <NextScript />
+          <Script src="https://www.googletagmanager.com/gtag/js?id=G-HDDCHC7YZH" strategy="afterInteractive" />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-HDDCHC7YZH');
+            `}
+          </Script>
         </body>
       </Html>
     )
