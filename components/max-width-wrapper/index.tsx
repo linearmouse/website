185023818467dfed1afe76c26322b7f16c442158
Jiahao Lu <lujjjh@gmail.com>
2023-07-05 10:12:56 +0800

    Update deps (#47)

--- components/max-width-wrapper/index.tsx
+++ components/max-width-wrapper/index.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 export type MaxWidthWrapperProps = {
-  maxWidth?: number | string
+  $maxWidth?: number | string
 }
 
 const MaxWidthWrapper = styled.div<MaxWidthWrapperProps>`
@@ -9,7 +9,7 @@ const MaxWidthWrapper = styled.div<MaxWidthWrapperProps>`
   margin-right: auto;
   padding-left: 4rem;
   padding-right: 4rem;
-  max-width: ${({ maxWidth }) => (typeof maxWidth === 'number' ? `${maxWidth}px` : maxWidth ?? `var(--max-width)`)};
+  max-width: ${({ $maxWidth }) => (typeof $maxWidth === 'number' ? `${$maxWidth}px` : $maxWidth ?? `var(--max-width)`)};
 `
 
 export default MaxWidthWrapper
