98932c2a4ac2bbcc32f4b5b204b509f6b59109cb
Jiahao Lu <lujjjh@gmail.com>
2024-10-07 00:31:37 +0800

    CN: Migrating from qcloud to aliyun

--- utils/config.js
+++ utils/config.js
@@ -1,3 +1,3 @@
 exports.CLUSTER_NAME = process.env.NEXT_PUBLIC_CLUSTER_NAME || process.env.CLUSTER_NAME || ''
 
-exports.isMainlandChinaCluster = exports.CLUSTER_NAME === 'qcloud'
+exports.isMainlandChinaCluster = exports.CLUSTER_NAME === 'qcloud' || exports.CLUSTER_NAME === 'aliyun'
