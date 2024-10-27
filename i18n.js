47e52efda1c37eb3210ab4489fca3ea16987c1f5
Jiahao Lu <lujjjh@gmail.com>
2024-10-27 13:39:45 +0800

    Fix build

--- i18n.js
+++ i18n.js
@@ -1,5 +1,42 @@
 const { isMainlandChinaCluster } = require('./utils/config')
 
+let locales = [
+  'af-ZA',
+  'ar-SA',
+  'ca-ES',
+  'cs-CZ',
+  'da-DK',
+  'de-DE',
+  'el-GR',
+  'en',
+  'es-ES',
+  'fi-FI',
+  'fr-FR',
+  'he-IL',
+  'hu-HU',
+  'it-IT',
+  'ja-JP',
+  'ko-KR',
+  'my-MM',
+  'nb-NO',
+  'nl-NL',
+  'no-NO',
+  'pl-PL',
+  'pt-BR',
+  'pt-PT',
+  'ro-RO',
+  'ru-RU',
+  'sk-SK',
+  'sr-SP',
+  'sv-SE',
+  'tr-TR',
+  'uk-UA',
+  'vi-VN',
+  'zh-CN',
+  'zh-HK',
+  'zh-TW'
+]
+
 let defaultLocale = 'en'
 
 if (isMainlandChinaCluster) {
@@ -9,6 +46,7 @@ if (isMainlandChinaCluster) {
 
 module.exports = {
   defaultLocale,
+  locales,
   pages: {
     '*': ['common'],
     '/': ['index']
