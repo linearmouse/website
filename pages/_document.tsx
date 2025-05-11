0c2f74924392bdfe4ea840b814b537b9bb28c272
Jiahao Lu <lujjjh@gmail.com>
2025-05-11 12:36:48 +0800

    Remove analytics scripts (#70)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,5 @@
 import Document, { DocumentContext, Head, Html, Main, NextScript } from 'next/document'
-import Script from 'next/script'
 import { ServerStyleSheet } from 'styled-components'
-import { isMainlandChinaCluster } from 'utils/config'
 
 export default class MyDocument extends Document {
   render() {
@@ -12,34 +10,10 @@ export default class MyDocument extends Document {
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
           <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
-          {isMainlandChinaCluster && (
-            <script
-              dangerouslySetInnerHTML={{
-                __html: `var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?b309ad83535f9f8875e32616df239396";
-  var s = document.getElementsByTagName("script")[0];
-  s.parentNode.insertBefore(hm, s);
-})();
-`
-              }}
-            />
-          )}
         </Head>
         <body>
           <Main />
           <NextScript />
-          <Script src="https://www.googletagmanager.com/gtag/js?id=G-HDDCHC7YZH" strategy="afterInteractive" />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-HDDCHC7YZH');
-            `}
-          </Script>
         </body>
       </Html>
     )
