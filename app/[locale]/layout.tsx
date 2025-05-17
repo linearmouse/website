cb3bc1a7f0da0728145995e52107fe4d26c234cc
Jiahao Lu <lujjjh@gmail.com>
2025-05-17 22:40:27 +0800

    Enable static rendering for i18n (#75)

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,10 +1,28 @@
 import GlobalStyle from 'components/GlobalStyle'
 import StyledComponentsRegistry from 'components/styled-components-regsitry'
 import { NextIntlClientProvider } from 'next-intl'
-import { getLocale } from 'next-intl/server'
+import { hasLocale } from 'next-intl'
+import { setRequestLocale } from 'next-intl/server'
+import { notFound } from 'next/navigation'
 
-export default async function Layout({ children }: { children: React.ReactNode }) {
-  const locale = await getLocale()
+import { routing } from 'i18n/routing'
+
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }))
+}
+
+export default async function Layout({
+  children,
+  params
+}: {
+  children: React.ReactNode
+  params: Promise<{ locale: string }>
+}) {
+  const { locale } = await params
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+  setRequestLocale(locale)
 
   return (
     <html lang={locale}>
