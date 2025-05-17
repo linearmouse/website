17f862c805db92c8dd7256ac1c3e8836ae4182f8
Jiahao Lu <lujjjh@gmail.com>
2025-05-17 23:12:52 +0800

    localePrefix -> as-needed

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -3,5 +3,6 @@ import i18nConfig from './index'
 
 export const routing = defineRouting({
   locales: i18nConfig.locales,
-  defaultLocale: i18nConfig.defaultLocale
+  defaultLocale: i18nConfig.defaultLocale,
+  localePrefix: 'as-needed'
 })
