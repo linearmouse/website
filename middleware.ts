666079740990216eac7d1374d74d277d9b5e7c41
Jiahao Lu <lujjjh@gmail.com>
2025-05-18 19:00:53 +0800

    Add `X-Rewrite-URL` header

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,20 @@
 import createMiddleware from 'next-intl/middleware'
 import { routing } from './i18n/routing'
+import type { NextMiddleware } from 'next/server'
 
-export default createMiddleware(routing)
+const i18nMiddleware = createMiddleware(routing)
+
+export const middleware = (async (request) => {
+  const response = i18nMiddleware(request)
+
+  const rewriteURL = response.headers.get('x-middleware-rewrite')
+  if (rewriteURL) {
+    // Cloudflare cache keys: https://developers.cloudflare.com/cache/how-to/cache-keys/
+    response.headers.set('x-rewrite-url', rewriteURL)
+  }
+
+  return response
+}) satisfies NextMiddleware
 
 export const config = {
   matcher: '/((?!api|trpc|_next|_vercel|.*\\..*).*)'
