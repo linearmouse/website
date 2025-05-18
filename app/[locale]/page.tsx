ce2dad85765ea09a6382c227bf5d783ce197b59a
Jiahao Lu <lujjjh@gmail.com>
2025-05-18 16:40:56 +0800

    Fix static generation

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,12 +1,13 @@
-import { getTranslations } from 'next-intl/server'
+import { getTranslations, setRequestLocale } from 'next-intl/server'
 
 import Features from 'components/features'
 import Footer from 'components/footer'
 import Header from 'components/header'
 import Hero from 'components/hero'
 
-export const generateMetadata = async () => {
-  const t = await getTranslations('index')
+export const generateMetadata = async ({ params }: { params: Promise<{ locale: string }> }) => {
+  const { locale } = await params
+  const t = await getTranslations({ locale, namespace: 'index' })
 
   return {
     title: t('title'),
@@ -14,7 +15,10 @@ export const generateMetadata = async () => {
   }
 }
 
-export default function Home() {
+export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
+  const { locale } = await params
+  setRequestLocale(locale)
+
   return (
     <>
       <Header />
