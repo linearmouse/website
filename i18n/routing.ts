bcd65fb6a8f70d6f0d4b939c6799c39fcc2858a3
Jiahao Lu <lujjjh@gmail.com>
2025-06-01 20:38:31 +0800

    Update locales for linearmouse.cn

--- i18n/routing.ts
+++ i18n/routing.ts
@@ -1,8 +1,58 @@
 import { defineRouting } from 'next-intl/routing'
-import i18nConfig from './index'
+
+const locales = [
+  'af-ZA',
+  'ar-SA',
+  'ca-ES',
+  'cs-CZ',
+  'da-DK',
+  'de-DE',
+  'el-GR',
+  'en',
+  'es-ES',
+  'fi-FI',
+  'fr-FR',
+  'he-IL',
+  'hu-HU',
+  'it-IT',
+  'ja-JP',
+  'ko-KR',
+  'my-MM',
+  'nb-NO',
+  'nl-NL',
+  'no-NO',
+  'pl-PL',
+  'pt-BR',
+  'pt-PT',
+  'ro-RO',
+  'ru-RU',
+  'sk-SK',
+  'sr-SP',
+  'sv-SE',
+  'tr-TR',
+  'uk-UA',
+  'vi-VN',
+  'zh-CN',
+  'zh-HK',
+  'zh-TW'
+]
+
+const defaultLocale = 'en'
 
 export const routing = defineRouting({
-  locales: i18nConfig.locales,
-  defaultLocale: i18nConfig.defaultLocale,
-  localePrefix: 'as-needed'
+  locales: locales,
+  defaultLocale: defaultLocale,
+  localePrefix: 'as-needed',
+  domains: [
+    {
+      domain: 'linearmouse.app',
+      defaultLocale: 'en',
+      locales: locales.filter((locale) => locale !== 'zh-CN')
+    },
+    {
+      domain: 'linearmouse.cn',
+      defaultLocale: 'zh-CN',
+      locales: ['zh-CN']
+    }
+  ]
 })
